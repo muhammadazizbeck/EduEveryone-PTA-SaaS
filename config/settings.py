e9e7c4dfc037bd6a55cbe29f9fa70e39a2a9b4d6
A1909bek <aa2004bek@gmail.com>
2025-06-06 20:25:45 +0500

    adding some changes

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG",default=False,cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["164.92.64.138"]
 
 
 # Application definition
