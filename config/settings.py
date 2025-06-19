9a8a3fb335c476a56c3fcce71c7c5f8a67fe0445
A1909bek <aa2004bek@gmail.com>
2025-06-19 12:08:46 +0500

    adding some changes

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG",default=False,cast=bool)
 
-ALLOWED_HOSTS = ['164.92.64.138', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['164.92.64.138', 'localhost', '127.0.0.1','edueveryone.uz']
 
 
 # Application definition
