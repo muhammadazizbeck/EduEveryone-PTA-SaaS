819b8c4176131ade72e6dcc099ac5b6a699c2fee
A1909bek <aa2004bek@gmail.com>
2025-07-03 11:26:43 +0500

    adding some changes

--- config/settings.py
+++ config/settings.py
@@ -150,7 +150,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images)dhdhd
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
